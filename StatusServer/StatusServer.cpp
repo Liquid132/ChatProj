b36ab77190ba62e023f4b4eab46c5bb1bfb5edc7
Liquid132 <1187105187@qq.com>
2026-04-14 13:53:49 +0800

    StatusServer: Initial commit

---
+++ StatusServer/StatusServer.cpp
@@ -0,0 +1,66 @@
+﻿#include <iostream>
+#include <json/json.h>
+#include <json/value.h>
+#include <json/reader.h>
+#include "const.h"
+#include "ConfigMgr.h"
+#include "hiredis.h"
+#include "RedisMgr.h"
+#include "MysqlMgr.h"
+#include "AsioIOServicePool.h"
+#include <iostream>
+#include <memory>
+#include <string>
+#include <thread>
+#include <boost/asio.hpp>
+#include "StatusServiceImpl.h"
+
+void RunServer() {
+    auto& cfg = ConfigMgr::Inst();
+
+    std::string server_address(cfg["StatusServer"]["Host"] + ":" + cfg["StatusServer"]["Port"]);
+    StatusServiceImpl service;
+
+    // 创建服务器入口
+    grpc::ServerBuilder builder;
+    // AddListeningPort监听一个TCP端口、添加服务，RegisterService表明不加密、不验证
+    builder.AddListeningPort(server_address, grpc::InsecureServerCredentials());
+    builder.RegisterService(&service);
+
+    // 构建、启动gRPC服务器
+    std::unique_ptr<grpc::Server> server(builder.BuildAndStart());
+    std::cout << "Server listening on " << server_address << std::endl;
+
+    // 创建Boost.Asio的io_context
+    boost::asio::io_context io_context;
+    // 创建signal_set用于捕获SIGINT，其中
+    // #define SIGINT          2   // interrupt
+    // #define SIGTERM         15  // Software termination signal from kill
+    boost::asio::signal_set signals(io_context, SIGINT, SIGTERM);
+
+    // 设置异步等待SIGINT信号，SIGINT使用ctrl+C触发
+    // SIGTERM杀进程触发
+    signals.async_wait([&server](const boost::system::error_code& error, int signal_number) {
+        if (!error) {
+            std::cout << "Shutting down server ... " << std::endl;
+            server->Shutdown(); //Shutdown 会让 server->Wait() 返回，从而停止服务
+        }
+        });
+
+    // 在单独的线程中运行io_context,监听系统信号
+    std::thread([&io_context]() { io_context.run(); }).detach();
+    // 阻塞主线程，等待shutdown（）被调用，服务器关闭
+    server->Wait();
+    io_context.stop(); // 停止io_context
+}
+
+int main(int argc, char** argv) {
+    try {
+        RunServer();
+    }
+    catch (std::exception const& e) {
+        std::cerr << "Error: " << e.what() << std::endl;
+        return EXIT_FAILURE;
+    }
+    return 0;
+}
---
+++ StatusServer/StatusServiceImpl.cpp
@@ -0,0 +1,135 @@
+#include "StatusServiceImpl.h"
+#include "ConfigMgr.h"
+#include "const.h"
+#include "RedisMgr.h"
+#include <climits>
+
+std::string generate_unique_string() {
+	// 创建UUID对象
+	boost::uuids::uuid uuid = boost::uuids::random_generator()();
+
+	// 将UUID转换为字符串（使用雪花算法生成）
+	std::string unique_string = to_string(uuid);
+
+	return unique_string;
+}
+
+// 处理GRPC获取聊天服务器请求
+Status StatusServiceImpl::GetChatServer(ServerContext* context, const GetChatServerReq* request, GetChatServerRsp* reply)
+{
+	std::string prefix("llfc status server has received :  ");
+	// 获取server信息，取出负载最低的服务器
+	const auto& server = getChatServer();
+	// 各项参数设置好后grpc底层会将reply返回给客户端
+	reply->set_host(server.host);
+	reply->set_port(server.port);
+	reply->set_error(ErrorCodes::Success);
+	reply->set_token(generate_unique_string());
+	insertToken(request->uid(), reply->token());
+	return Status::OK;
+}
+
+StatusServiceImpl::StatusServiceImpl()
+{
+	auto& cfg = ConfigMgr::Inst();
+	auto server_list = cfg["chatservers"]["Name"];
+
+	std::vector<std::string> words;
+
+	std::stringstream ss(server_list);
+	std::string word;
+
+	while (std::getline(ss, word, ',')) {
+		words.push_back(word);
+	}
+
+	for (auto& word : words) {
+		if (cfg[word]["Name"].empty()) {
+			continue;
+		}
+
+		ChatServer server;
+		server.port = cfg[word]["Port"];
+		server.host = cfg[word]["Host"];
+		server.name = cfg[word]["Name"];
+		_servers[server.name] = server;
+	}
+
+}
+
+ChatServer StatusServiceImpl::getChatServer() {
+	std::lock_guard<std::mutex> guard(_server_mtx);
+	// 取出第一个服务器作为负载量最低的Server
+	auto minServer = _servers.begin()->second;
+	// 在redis中根据LOGIN_COUNT和name寻找server对应最小数量
+	auto count_str = RedisMgr::GetInstance()->HGet(LOGIN_COUNT, minServer.name);
+	if (count_str.empty()) {
+		//不存在则默认设置为最大
+		minServer.con_count = INT_MAX;
+	}
+	else {
+		minServer.con_count = std::stoi(count_str);
+	}
+
+
+	// 使用范围基于for循环
+	for ( auto& server : _servers) {
+		
+		if (server.second.name == minServer.name) {
+			continue;
+		}
+
+		auto count_str = RedisMgr::GetInstance()->HGet(LOGIN_COUNT, server.second.name);
+		if (count_str.empty()) {
+			server.second.con_count = INT_MAX;
+		}
+		else {
+			server.second.con_count = std::stoi(count_str);
+		}
+
+		if (server.second.con_count < minServer.con_count) {
+			minServer = server.second;
+		}
+	}
+
+	// 使用范围基于for循环，找到连接数量最小的Server（压力最小）
+	for (const auto& server : _servers) {
+		if (server.second.con_count < minServer.con_count) {
+			minServer = server.second;
+		}
+	}
+
+	return minServer;
+}
+
+Status StatusServiceImpl::Login(ServerContext* context, const LoginReq* request, LoginRsp* reply)
+{
+	auto uid = request->uid();
+	auto token = request->token();
+
+	std::string uid_str = std::to_string(uid);
+	std::string token_key = USERTOKENPREFIX + uid_str;
+	std::string token_value = "";
+	bool success = RedisMgr::GetInstance()->Get(token_key, token_value);
+	if (success) {
+		reply->set_error(ErrorCodes::UidInvalid);
+		return Status::OK;
+	}
+	
+	if (token_value != token) {
+		reply->set_error(ErrorCodes::TokenInvalid);
+		return Status::OK;
+	}
+	reply->set_error(ErrorCodes::Success);
+	reply->set_uid(uid);
+	reply->set_token(token);
+	return Status::OK;
+}
+
+void StatusServiceImpl::insertToken(int uid, std::string token)
+{
+	std::string uid_str = std::to_string(uid);
+	std::string token_key = USERTOKENPREFIX + uid_str;
+	RedisMgr::GetInstance()->Set(token_key, token);
+}
+
